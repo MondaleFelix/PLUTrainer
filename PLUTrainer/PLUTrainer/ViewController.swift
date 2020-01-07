4f805b73ef74cc5043edb40680afaddb5640eb5c
MondaleFelix <mondalefelix@yahoo.com>
2020-01-07 05:27:32 -0800

    linked keyboard buttons to code

--- PLUTrainer/PLUTrainer/ViewController.swift
+++ PLUTrainer/PLUTrainer/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  PLUTrainer
 //
-//  Created by Mondale on 1/6/20.
+//  Created by Mondale on 1/7/20.
 //  Copyright © 2020 Mondale. All rights reserved.
 //
 
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func keyboardButtonPressed(_ sender: UIButton) {
+        print(sender.currentTitle!)
+    }
 }
-
