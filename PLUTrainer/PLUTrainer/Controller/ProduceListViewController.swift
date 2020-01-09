e9008277656f2bd1e9e681c98d39f50941a984e9
MondaleFelix <mondalefelix@yahoo.com>
2020-01-09 05:48:22 -0800

    fixed bug where table view cell was empty

--- PLUTrainer/PLUTrainer/Controller/ProduceListViewController.swift
+++ PLUTrainer/PLUTrainer/Controller/ProduceListViewController.swift
@@ -2,20 +2,25 @@
 //  ProduceListViewController.swift
 //  PLUTrainer
 //
-//  Created by Mondale on 1/8/20.
+//  Created by Mondale on 1/9/20.
 //  Copyright © 2020 Mondale. All rights reserved.
 //
 
 import UIKit
 
-class ProduceListViewController : UIViewController {
-    
-    var produce = [Produce]()
+class ProduceListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProduceItem", for: indexPath)
+        return cell
+    }
 }
