b480eb7591e0e4974e106d3d87cecd765f073c93
MondaleFelix <mondalefelix@yahoo.com>
2020-01-09 03:38:53 -0800

    added table view, navigation controller, and cell

---
+++ PLUTrainer/PLUTrainer/Controller/ProduceCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProduceCell.swift
+//  PLUTrainer
+//
+//  Created by Mondale on 1/9/20.
+//  Copyright © 2020 Mondale. All rights reserved.
+//
+
+import UIKit
+
+class ProduceCell: UITableViewCell {
+
+
+    @IBOutlet weak var produceImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}
---
+++ PLUTrainer/PLUTrainer/Controller/ProduceListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProduceListViewController.swift
+//  PLUTrainer
+//
+//  Created by Mondale on 1/8/20.
+//  Copyright © 2020 Mondale. All rights reserved.
+//
+
+import UIKit
+
+class ProduceListViewController : UIViewController {
+    
+    var produce = [Produce]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
